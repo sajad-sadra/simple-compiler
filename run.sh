38b79e7b3b42a9baafdbe60d7927806bd93cd517
sajad sadrayieh <mss.sadra137812133@gmail.com>
2021-01-21 22:53:16 +0330

    run bash script pruned

--- run.sh
+++ run.sh
@@ -1,8 +1,4 @@
-rm zpp > /dev/null 2>&1
-rm main.tab.c > /dev/null 2>&1
-rm main.tab.h > /dev/null 2>&1
-rm lex.yy.c > /dev/null 2>&1
-
+rm $(cat .gitignore) > /dev/null 2>&1
 
 bison -d main.y
 flex main.l
