999c09b6897f8c25a9c15f25fccc1f383e45cafb
sajad sadrayieh <mss.sadra137812133@gmail.com>
2021-01-21 22:02:59 +0330

    bash output hided

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
-rm zpp > /dev/null
-rm main.tab.c > /dev/null
-rm main.tab.h > /dev/null
-rm lex.yy.c > /dev/null
+rm zpp > /dev/null 2>&1
+rm main.tab.c > /dev/null 2>&1
+rm main.tab.h > /dev/null 2>&1
+rm lex.yy.c > /dev/null 2>&1
 
 
 bison -d main.y
